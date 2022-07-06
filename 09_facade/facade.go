e66746389fd7f0a7e523df69c1574df9098c0610
inksnw <inknsw@gmail.com>
2022-07-06 10:47:06 +0800

    门面模式

---
+++ 09_facade/facade.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+// IUser 用户接口
+type IUser interface {
+	Login(phone int, code int) (*User, error)
+	Register(phone int, code int) (*User, error)
+}
+
+// IUserFacade 门面模式
+type IUserFacade interface {
+	LoginOrRegister(phone int, code int) error
+}
+
+// User 用户
+type User struct {
+	Name string
+}
+
+// UserService UserService
+type UserService struct{}
+
+// Login 登录
+func (u UserService) Login(phone int, code int) (*User, error) {
+	// 校验操作 ...
+	return &User{Name: "test login"}, nil
+}
+
+// Register 注册
+func (u UserService) Register(phone int, code int) (*User, error) {
+	// 校验操作 ...
+	// 创建用户
+	return &User{Name: "test register"}, nil
+}
+
+// LoginOrRegister 登录或注册
+func (u UserService) LoginOrRegister(phone int, code int) (*User, error) {
+	user, err := u.Login(phone, code)
+	if err != nil {
+		return nil, err
+	}
+
+	if user != nil {
+		return user, nil
+	}
+
+	return u.Register(phone, code)
+}
+
+func main() {
+	service := UserService{}
+	user1, _ := service.Login(13001010101, 1234)
+	user2, _ := service.LoginOrRegister(13001010101, 1234)
+	user3, _ := service.Register(13001010101, 1234)
+	fmt.Println(user1, user2, user3)
+
+}
